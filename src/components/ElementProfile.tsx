7542dd867105d3615e2df3f7f6a2a424b086ec77
vladimir.chikaidze <66840575+skulidropek@users.noreply.github.com>
2024-07-13 16:25:45 +0400

    Убрал лишнюю кнопку раскрытия компонентов

--- src/components/ElementProfile.tsx
+++ src/components/ElementProfile.tsx
@@ -1,5 +1,5 @@
-import React, { useState, forwardRef } from 'react';
-import { Button, Collapse, Card } from 'react-bootstrap';
+import React, { forwardRef } from 'react';
+import { Button, Card } from 'react-bootstrap';
 import { FaPlus, FaArrowsAlt } from 'react-icons/fa';
 import ElementSetting from './ElementSetting';
 import CuiElement from '../models/CuiElement';
@@ -12,45 +12,38 @@ interface ElementProfileProps {
 }
 
 const ElementProfile = forwardRef<HTMLDivElement, ElementProfileProps>(({ element, onChange, onAddChild, onToggleFill }, ref) => {
-  const [open, setOpen] = useState(false);
-
   return (
     <Card className="mb-2">
       <Card.Header className="d-flex justify-content-between align-items-center">
         <span>{element.name}</span>
-        <Button variant="link" size="sm" onClick={() => setOpen(!open)}>
-          {open ? '-' : '+'}
-        </Button>
       </Card.Header>
-      <Collapse in={open}>
-        <Card.Body ref={ref}>
-          <div className="d-flex mb-2">
-            <Button variant="success" size="sm" className="mr-2" onClick={onAddChild}>
-              <FaPlus />
-            </Button>
-            <Button variant="primary" size="sm" onClick={onToggleFill}>
-              <FaArrowsAlt />
-            </Button>
-          </div>
-          <ElementSetting element={element} onChange={onChange} />
-          <div className="mt-3">
-            <b>Components</b>
-            {element.components?.map((component: any, index: number) => (
-              <Card key={index} className="mb-2">
-                <Card.Header className="d-flex justify-content-between align-items-center">
-                  <span>{component.name}</span>
-                  <Button variant="link" size="sm">
-                    <FaPlus />
-                  </Button>
-                </Card.Header>
-                <Card.Body>
-                  <ElementSetting element={component} onChange={onChange} />
-                </Card.Body>
-              </Card>
-            ))}
-          </div>
-        </Card.Body>
-      </Collapse>
+      <Card.Body ref={ref}>
+        <div className="d-flex mb-2">
+          <Button variant="success" size="sm" className="mr-2" onClick={onAddChild}>
+            <FaPlus />
+          </Button>
+          <Button variant="primary" size="sm" onClick={onToggleFill}>
+            <FaArrowsAlt />
+          </Button>
+        </div>
+        <ElementSetting element={element} onChange={onChange} />
+        <div className="mt-3">
+          <b>Components</b>
+          {element.components?.map((component: any, index: number) => (
+            <Card key={index} className="mb-2">
+              <Card.Header className="d-flex justify-content-between align-items-center">
+                <span>{component.name}</span>
+                <Button variant="link" size="sm">
+                  <FaPlus />
+                </Button>
+              </Card.Header>
+              <Card.Body>
+                <ElementSetting element={component} onChange={onChange} />
+              </Card.Body>
+            </Card>
+          ))}
+        </div>
+      </Card.Body>
     </Card>
   );
 });
