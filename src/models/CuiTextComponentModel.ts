31e00773424de6627604491defa0622eb13dbcf0
vladimir.chikaidze <66840575+skulidropek@users.noreply.github.com>
2024-07-28 21:10:18 +0400

    Сделал что бы цвет был обёрнут в ковычки

--- src/models/CuiTextComponentModel.ts
+++ src/models/CuiTextComponentModel.ts
@@ -51,7 +51,7 @@ export default class CuiTextComponentModel implements ICuiComponent {
     
     const properties = [
       this.fadeIn !== null ? `FadeIn = ${this.fadeIn}` : null,
-      this.color !== null ? `Color = ${this.color}` : null,
+      this.color !== null ? `Color = "${this.color}"` : null,
       this.text !== undefined ? `Text = "${this.text}"` : null,
       this.fontSize !== undefined ? `FontSize = ${this.fontSize}` : null,
       this.font !== undefined ? `Font = "${this.font}"` : null,
