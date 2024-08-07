1b89f4566b02e920e52a55ade2f78c7466758318
vladimir.chikaidze <66840575+skulidropek@users.noreply.github.com>
2024-08-07 17:08:04 +0400

    ДОбавил выбор изображений

--- src/declarations.d.ts
+++ src/declarations.d.ts
@@ -1,6 +1,17 @@
-// declarations.d.ts
-declare module '*.svg' {
-    const content: any;
-    export default content;
+declare module '*.png';
+declare module '*.jpg';
+declare module '*.jpeg';
+declare module '*.svg';
+
+declare namespace __WebpackModuleApi {
+  interface RequireContext {
+    (id: string): any;
+    keys(): string[];
+    resolve(id: string): string;
+    id: string;
   }
-  
+}
+
+declare const require: {
+  context: (path: string, deep?: boolean, filter?: RegExp) => __WebpackModuleApi.RequireContext;
+};
